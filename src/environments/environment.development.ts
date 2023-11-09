d6742411ade806011840a58ff19cf00e78940231
Bhavin Panchal <panchalbhavin.007@gmail.com>
2023-11-09 11:35:45 +0530

    feat: loanaccount base url updated

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     customerAccountBaseUrl: "http://52.90.228.22:8080/",
     savingsAccountBaseUrl: "http://52.90.228.22:8081/",
-    loanAccountBaseUrl: "http://52.90.228.22:8082/"
+    loanAccountBaseUrl: "http://52.90.228.22:8083/"
 };
--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -3,5 +3,5 @@ export const environment = {
     // replace with prod urls in future
     customerAccountBaseUrl: "http://52.90.228.22:8080/", 
     savingsAccountBaseUrl: "http://52.90.228.22:8081/",
-    loanAccountBaseUrl: "http://52.90.228.22:8082/"
+    loanAccountBaseUrl: "http://52.90.228.22:8083/"
 };
